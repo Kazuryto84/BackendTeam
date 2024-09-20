f036d6584187bd915c05bc378974276ea7b938b9
Kazuryto84 <117948926+Kazuryto84@users.noreply.github.com>
2024-09-20 17:08:07 -0500

    fixing status

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3200;
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.json("Api loading OK");
+  });
+
 app.get('/data', (req, res) => {
   res.json([
     { id: 2, name: 'Juan', number: 1, position: 1 },
